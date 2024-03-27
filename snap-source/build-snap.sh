17c56cbbd509a6d6e9573acc86af5147eaff308c
devizer <devizer@users.github.io>
2024-03-27 12:17:31 +0200

    Build snap

---
+++ snap-source/build-snap.sh
@@ -0,0 +1,10 @@
+set -eu
+chmod +x snap/hooks/configure
+chmod +x w3diskmark
+ver=$(cat bin-x64/VERSION); sed -i "/^version\:/d" snap/snapcraft.yaml; printf "\n\nversion: $ver\n" >> snap/snapcraft.yaml; echo "SNAP VERSION: $ver"
+for arch in x64 arm64 arm; do cp -v w3diskmark bin-$arch/; done
+pkill w3diskmark || true
+sudo snap remove --purge w3diskmark || true
+rm -f w3diskmark*.snap || true
+# snapcraft clean; 
+time snapcraft
---
+++ snap-source/download-binaries.sh
@@ -0,0 +1,11 @@
+work="$(pwd)"
+pushd .
+for arch in x64 arm64 arm; do
+  mkdir -p $work/bin-$arch
+  Say "$arch --> $work/bin-$arch"
+  cd $work/bin-$arch && rm -rf *
+  url=https://github.com/devizer/w3top-bin/releases/latest/download/w3top-linux-$arch.tar.xz
+  try-and-retry curl -kfSL -o /tmp/w3diskmark-$arch.tar.xz "$url"
+  tar xJf /tmp/w3diskmark-$arch.tar.xz
+done
+popd.
