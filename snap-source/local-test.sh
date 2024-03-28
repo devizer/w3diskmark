620046e2543114d48544a8a1c380ae9aa1bac864
devizer <devizer@users.github.io>
2024-03-28 23:21:19 +0200

    Build snap, classic local test, strict on pipeline

--- snap-source/local-test.sh
+++ snap-source/local-test.sh
@@ -3,12 +3,14 @@ work=$HOME/build/w3diskmark-local
 if [[ ! -d $work ]]; then git clone https://github.com/devizer/w3diskmark $work; fi
 # Alter upgrade: rm -rf $work/*
 cd $work
+git reset --hard; git pull; 
 cd snap-source; 
 if [[ ! -d bin-x64 ]]; then bash -e download-binaries.sh || rm -rf bin-a*; fi
-git reset --hard; git pull; time bash -e build-snap.sh;
+bash -e patch-snapcraft.sh "snap/snapcraft.yaml" "confinement" "classic"
+time bash -e build-snap.sh;
 sudo snap install w3diskmark_*.snap --dangerous --classic
 sudo snap set w3diskmark http.port=5555
 sudo snap restart w3diskmark
-sleep 5
+sleep 9
 sudo snap logs -n=1000 w3diskmark.service
 
