f05e54de65f1924a5ae320c7eb3f3d98d4f5c871
devizer <devizer@users.github.io>
2024-03-28 15:29:17 +0200

    Build snap, fix confinement

--- snap-source/local-test.sh
+++ snap-source/local-test.sh
@@ -2,8 +2,7 @@ set -eu; set -o pipefail
 work=$HOME/build/w3diskmark-local
 if [[ ! -d $work ]]; then git clone https://github.com/devizer/w3diskmark $work; fi
 cd $work
-git pull
 cd snap-source; 
 if [[ ! -d bin-x64 ]]; then bash -e download-binaries.sh || rm -rf bin-a*; fi
-time bash -e build-snap.sh;
+git pull; time bash -e build-snap.sh;
 
