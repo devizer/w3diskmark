777576a2b03c2072ea80707f4b9a0e2239f1b6d0
devizer <devizer@users.github.io>
2024-03-28 02:37:27 +0200

    Build snap: core20, daemon, plugs v15

--- snap-source/download-binaries.sh
+++ snap-source/download-binaries.sh
@@ -6,6 +6,6 @@ for arch in x64; do # SKIP: arm64 arm
   cd $work/bin-$arch && rm -rf *
   url=https://github.com/devizer/w3top-bin/releases/latest/download/w3top-linux-$arch.tar.gz
   try-and-retry curl -ksfSL -o /tmp/w3diskmark-$arch.tar.xz "$url"
-  tar xJf /tmp/w3diskmark-$arch.tar.xz
+  tar xzf /tmp/w3diskmark-$arch.tar.xz
 done
 popd
