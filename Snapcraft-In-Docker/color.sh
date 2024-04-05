11e0bef11e6bc80c58a94672db15d96932793418
devizer <devizer@users.github.io>
2024-04-06 01:47:20 +0300

    snapcraft in docker v35, MiltiArch, no core16, logs on smoke test, missing pushd and [[ in core18

--- Snapcraft-In-Docker/color.sh
+++ Snapcraft-In-Docker/color.sh
@@ -1,4 +1,5 @@
-EchoColored() { ANSI_NC='\033[0m'; ANSI_Color_Green='\033[1;32m'; ANSI_Color_Red='\033[1;31m'; ANSI_Color_Yellow='\033[1;33m'; color="$(eval "echo \$ANSI_Color_${1}")"; shift; if [ -z "$BASH_VERSION" ]; then echo "${color}$*${ANSI_NC}"; else echo -e "${color}$*${ANSI_NC}"; fi; }; \
+set -eu
+EchoColored() { ANSI_NC='\033[0m'; ANSI_Color_Green='\033[1;32m'; ANSI_Color_Red='\033[1;31m'; ANSI_Color_Yellow='\033[1;33m'; color="$(eval "echo \$ANSI_Color_${1}")"; shift; if [ -z "${BASH_VERSION:-}" ]; then echo "${color}$*${ANSI_NC}"; else echo -e "${color}$*${ANSI_NC}"; fi; }; \
 EchoRedError() { EchoColored Red "\n$*\n"; }; \
 EchoColored Green Hello. "I'm" Running
 EchoColored Red "Error. Abort"
